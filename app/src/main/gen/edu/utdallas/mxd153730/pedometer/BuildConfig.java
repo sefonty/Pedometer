beaaa389441ef6e311d468ffc0817fbe96681054
Melissa Dagley <Mxd153730@utdallas.edu>
2016-11-09 09:20:09 -0600

    Created the goal activity screen. You can click new goal on the main screen to open it, and you can click cancel on the goal screen to return to the main screen.

---
+++ app/src/main/gen/edu/utdallas/mxd153730/pedometer/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package edu.utdallas.mxd153730.pedometer;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
